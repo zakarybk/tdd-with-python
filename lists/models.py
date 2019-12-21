ac48e08820d16ea0ce94b0aea2ebbba814adf7d8
Zak <>
2019-12-21 15:50:59 +0000

    Model for list Items and associated migration

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+# Have to run - python manage.py makemigrations - to update stuff to make this work
+class Item(models.Model):
+	text = models.TextField(default='')
