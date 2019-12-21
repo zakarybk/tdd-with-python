f91437f8bb5ef4b4a6240074e4177e627c6a3319
Zak <>
2019-12-21 21:42:58 +0000

    New URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -51,11 +51,6 @@ class ItemModelTest(TestCase):
 
 class ListViewTest(TestCase):
 
-	def test_displays_all_list_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
-
-		response = self.client.get('/')
-
-		self.assertContains(response, 'itemey 1')
-		self.assertContains(response, 'itemey 2')
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
