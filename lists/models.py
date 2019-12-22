beb29a1a637d7a480674ddf2d1292394a68a2207
Zak <>
2019-12-22 21:56:44 +0000

    118

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
+class List(models.Model):
+	pass
+
 # Have to run - python manage.py makemigrations - to update stuff to make this work
 class Item(models.Model):
 	text = models.TextField(default='')
+	list = models.ForeignKey(List, default=None)
