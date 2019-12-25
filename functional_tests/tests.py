1f83c2a5cb277f3427f0b9325b17bf0cc8de2e19
Zak <>
2019-12-25 19:43:43 +0000

    Refactor templates to use a base template - had a problem with 'end block' being two words instead of 'endblock', no easy way to debug this even with TDD as we made so many changes at once....

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -111,7 +111,7 @@ class NewVisitorTest(LiveServerTestCase):
 		# She notices the input box is nicely centered
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.location['width'] / 2,
+			inputbox.location['x'] + inputbox.size['width'] / 2,
 			512,
 			delta=10
 		)
