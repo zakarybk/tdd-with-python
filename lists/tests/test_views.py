d90d3b9109b5ea36880b10937a25c530d4113b10
Zak <zakarybk@users.noreply.github.com>
2019-12-27 22:55:39 +0000

    Use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,13 +6,19 @@ from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
+from list.forms import ItemForm
 
 # Create your tests here.
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
+	def test_uses_home_template(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+
+	def test_home_page_uses_item_form(self):
+		response = self.client.get('/')
+		self.assertIsInstance(response.context['form'], ItemForm)
+
 
 class ListViewTest(TestCase):
 
