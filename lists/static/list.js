d4433f8d0d82ce5089ab832ef15a7e9c64c37ff3
zb201421 <zakarybk@users.noreply.github.com>
2019-12-28 14:21:15 +0000

    Duplicate entries error is hidden on keypress and click

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,9 @@
-var initialise = function() {
-	$('input[name="text"').on('keypress', function(){
+window.Superlists = {};
+window.Superlists.initialise = function() {
+	$('input[name="text"]').on('keypress', function(){
+		$('.has-error').hide();
+	});
+	$('input[name="text"]').on('click', function(){
 		$('.has-error').hide();
 	});
 };
