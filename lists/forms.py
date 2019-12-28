28584f1e9041b8fe23d9b4c1a2f35b6289b07b87
zb201421 <zakarybk@users.noreply.github.com>
2019-12-28 11:49:18 +0000

    Form validation

--- lists/forms.py
+++ lists/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 # For more complex forms see - https://django-crispy-forms.readthedocs.org/
 class ItemForm(forms.models.ModelForm):
@@ -23,3 +25,16 @@ class ItemForm(forms.models.ModelForm):
 		error_messages = {
 			'text': {'required': EMPTY_ITEM_ERROR}
 		}
+
+class ExistingListItemForm(ItemForm):
+
+	def __init__(self, for_list, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.instance.list = for_list
+
+	def validate_unique(self):
+		try:
+			self.instance.validate_unique()
+		except ValidationError as e:
+			e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+			self._update_errors(e)
