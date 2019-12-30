00a1465bc97f85b104670e24ea52d0073eed24b8
zb201421 <zakarybk@users.noreply.github.com>
2019-12-30 10:01:15 +0000

    Email login

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -12,9 +12,10 @@ class FunctionalTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		staging_server = os.environ.get('STAGING_SERVER')
-		if staging_server:
-			self.live_server_url = 'http://' + staging_server
+		self.staging_server = os.environ.get('STAGING_SERVER')
+		if self.staging_server:
+			self.live_server_url = 'http://' + self.staging_server
+			reset_database(self.staging_server)
 
 	def tearDown(self):
 		self.browser.quit()
